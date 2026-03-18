e04527d377ab53e2c01242883422653e87ce540c
Christoph Hartmann <christoph.hartmann@aeb.com>
2026-03-18 15:21:19 +0100

    fix: update WrappedResponseWriter

--- middlewares/logging.go
+++ middlewares/logging.go
@@ -1,17 +1,18 @@
 package middlewares
 
 import (
-	"github.com/chriss-de/mux-middlewares/utilities"
 	"log/slog"
 	"net/http"
 	"time"
+
+	"github.com/chriss-de/mux-middlewares/utilities"
 )
 
 // Logging calls slog to log the http request
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// wrap the http.ResponseWriter so we can access the http.Status in the defer()
-		wrw := utilities.NewWrappedResponseWriter(w)
+		wrw := utilities.NewWrappedResponseWriter(w, r.ProtoMajor)
 
 		reqStart := time.Now()
 
@@ -25,7 +26,7 @@ func Logging(next http.Handler) http.Handler {
 				slog.String("method", r.Method),
 				slog.Int("status", wrw.Status()),
 				slog.String("latency", time.Since(reqStart).String()),
-				slog.Int64("size", wrw.BytesWritten()),
+				slog.Int("size", wrw.BytesWritten()),
 				slog.String("user_agent", r.UserAgent()),
 			)
 		}()
